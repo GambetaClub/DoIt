fd43c2ac6944c71171fe6df66518aed5a8505eb7
GambetaClub <marigyt.maj@gmail.com>
2023-12-22 13:06:31 -0600

    Still trying to fix.

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { NextResponse } from 'next/server'
-import { clerkClient } from '@clerk/nextjs'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
  
@@ -45,7 +45,7 @@ export async function POST(req: Request) {
     }) as WebhookEvent
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occurred', {
+    return new Response('Error occured', {
       status: 400
     })
   }
@@ -54,20 +54,19 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
-  if (eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data
-    
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     }
 
-
-    const newUser = await createUser(user)
+    const newUser = await createUser(user);
 
     if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
@@ -77,33 +76,31 @@ export async function POST(req: Request) {
       })
     }
 
-    return NextResponse.json({message:  'OK', user: newUser})
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
   if (eventType === 'user.updated') {
-    const { id, image_url, first_name, last_name, username } = evt.data
-    
+    const {id, image_url, first_name, last_name, username } = evt.data
+
     const user = {
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url
+      firstName: first_name,
+      lastName: last_name,
+      username: username!,
+      photo: image_url,
     }
 
     const updatedUser = await updateUser(id, user)
 
-    return NextResponse.json({message:  'OK', user: updatedUser})
+    return NextResponse.json({ message: 'OK', user: updatedUser })
   }
 
   if (eventType === 'user.deleted') {
-
-    const {id} = evt.data
+    const { id } = evt.data
 
     const deletedUser = await deleteUser(id!)
 
-    return NextResponse.json({message: 'OK', user: deletedUser})
+    return NextResponse.json({ message: 'OK', user: deletedUser })
   }
-
  
   return new Response('', { status: 200 })
 }
