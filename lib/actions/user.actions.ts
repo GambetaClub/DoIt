454111d9c09a170dc49095fb0aa0d201611f2548
GambetaClub <marigyt.maj@gmail.com>
2024-05-26 11:19:28 -0500

    Deleting events from user when it gets deleted.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { revalidatePath } from 'next/cache'
 
 import { connectToDatabase } from '@/lib/database'
 import User from '@/lib/database/models/user.model'
+import Event from '@/lib/database/models/event.model'
+
 import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
@@ -56,11 +58,33 @@ export async function deleteUser(clerkId: string) {
       throw new Error('User not found')
     }
 
+    deleteEventsFromUser(clerkId)
+
     const deletedUser = await User.findByIdAndDelete(userToDelete._id)
     revalidatePath('/')
 
     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
   } catch (error) {
     handleError(error)
+  }
+}
+
+
+export async function deleteEventsFromUser(clerkId: string) {
+  try {
+    await connectToDatabase();
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error('User not found');
+    }
+
+    // Delete events associated with the user
+    await Event.deleteMany({ organizer: userToDelete._id });
+
+  } catch (error) {
+    handleError(error);
   }
 }
